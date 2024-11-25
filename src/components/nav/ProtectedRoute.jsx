eee90c4bf34b19977470305e5670c5b36aa5646f
Emilio Sanchez <emilio@192.168.1.10>
2024-11-25 10:51:02 -0600

    details progress

--- src/components/nav/ProtectedRoute.jsx
+++ src/components/nav/ProtectedRoute.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from 'react-router-dom'
 import { useUser } from '../../hooks/react-query/user/useUser'
 
 function ProtectedRoute({ children }) {
-  const { data: user } = useUser()
+  const { data: user, isPending } = useUser()
   const navigate = useNavigate()
 
   useEffect(() => {
-    if (!user) {
+    if (!user && !isPending) {
       navigate('/login')
     }
-  }, [user, navigate])
+  }, [user, isPending, navigate])
 
   if (!user) {
     // You can also render a loading indicator or null while checking user status
--- src/pages/ListDetailsPage.jsx
+++ src/pages/ListDetailsPage.jsx
@@ -1,13 +1,102 @@
 import { useParams } from 'react-router-dom'
 import AppLayout from '../components/layout/AppLayout'
 import PageLayout from '../components/layout/PageLayout'
+import useCurrentWorkspace from '../hooks/useCurrentWorkspace'
+import { useEmailLists } from '../hooks/react-query/email-lists/useEmailLists'
+import { Card, CardBody, CardHeader, Chip, Divider } from '@nextui-org/react'
+import { PiFileCsvDuotone } from 'react-icons/pi'
+import { useEffect, useState } from 'react'
+import { RiCircleFill } from 'react-icons/ri'
 
 function ListDetailsPage() {
+  const [currentWorkspace] = useCurrentWorkspace()
+  const { data } = useEmailLists(currentWorkspace)
   const { id } = useParams()
+  const [list, setList] = useState(null)
+
+  useEffect(() => {
+    if (data && id) {
+      const list = data.find((list) => list.id === id)
+      setList(list)
+    }
+  }, [id, data])
+
+  const getKeyColor = (key) => {
+    switch (key) {
+      case 'deliverable':
+        return 'success'
+      case 'undeliverable':
+        return 'danger'
+      case 'risky':
+        return 'warning'
+      default:
+        return 'default-500'
+    }
+  }
 
   return (
     <AppLayout>
-      <PageLayout backBtn>this is a details pagefor {id}</PageLayout>
+      <PageLayout
+        backBtn
+        maxW="3xl"
+        title={list?.name || 'List Details'}
+        primaryAction="Export"
+      >
+        <div className="w-full flex gap-6">
+          <Card className="basis-1 grow">
+            <CardHeader>Details</CardHeader>
+            <Divider />
+            <CardBody className="flex flex-col gap-3">
+              <div className="w-full flex gap-3 justify-between">
+                <div className="flex gap-2 items-center">
+                  <PiFileCsvDuotone
+                    fontSize="1.6rem"
+                    className="text-default-600"
+                  />
+                  <span className="font-medium">{list?.name}</span>
+                </div>
+                <Chip
+                  variant="flat"
+                  color={
+                    (list?.status === 'pending' && 'default') ||
+                    (list?.status === 'processing' && 'primary') ||
+                    (list?.status === 'completed' && 'success') ||
+                    (list?.status === 'error' && 'danger')
+                  }
+                >
+                  {list?.status}
+                </Chip>
+              </div>
+              {list &&
+                Intl.DateTimeFormat(navigator.language, {
+                  dateStyle: 'short',
+                }).format(new Date(list?.created_at))}
+            </CardBody>
+          </Card>
+          <Card className="basis-1 grow">
+            <CardHeader>Overview</CardHeader>
+            <Divider />
+            <CardBody className="flex flex-col gap-2">
+              {list &&
+                ['deliverable', 'risky', 'undeliverable', 'unknown'].map(
+                  (key) => {
+                    return (
+                      <div key={key} className="flex gap-3 justify-between">
+                        <div className="flex gap-2 items-center">
+                          <RiCircleFill
+                            className={`text-xs text-${getKeyColor(key)}`}
+                          />
+                          <span>{key}</span>
+                        </div>
+                        <span>{list?.summary[key]}</span>
+                      </div>
+                    )
+                  }
+                )}
+            </CardBody>
+          </Card>
+        </div>
+      </PageLayout>
     </AppLayout>
   )
 }
