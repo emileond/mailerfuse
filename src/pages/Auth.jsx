818dcb58786e38e08b2c479f4c86c71345cad2d9
Emilio Sanchez <sonarart@gmail.com>
2025-01-08 21:55:40 -0600

    adding members during onboarding

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,30 +1,30 @@
-import AuthForm from '../components/auth/AuthForm'
-import { useEffect } from 'react'
-import { useUser } from '../hooks/react-query/user/useUser'
-import { useWorkspaces } from '../hooks/react-query/teams/useWorkspaces'
-import { useNavigate } from 'react-router-dom'
+import AuthForm from '../components/auth/AuthForm';
+import { useEffect } from 'react';
+import { useUser } from '../hooks/react-query/user/useUser';
+import { useWorkspaces } from '../hooks/react-query/teams/useWorkspaces';
+import { useNavigate } from 'react-router-dom';
 
 function AuthPage({ authMode = 'login' }) {
-  const { data: user } = useUser()
-  const { data: workspaces } = useWorkspaces(user)
-  const navigate = useNavigate()
+    const { data: user } = useUser();
+    const { data: workspaces } = useWorkspaces(user);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user && workspaces) {
-      if (workspaces.length > 0) {
-        navigate('/dashboard')
-      } else {
-        console.log('no workspaces', workspaces, user)
-        navigate('/onboarding')
-      }
-    }
-  }, [user, workspaces, navigate])
+    useEffect(() => {
+        if (user && workspaces) {
+            if (!workspaces.length > 0) {
+                navigate('/dashboard');
+            } else {
+                console.log('no workspaces', workspaces, user);
+                navigate('/onboarding');
+            }
+        }
+    }, [user, workspaces, navigate]);
 
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <AuthForm viewMode={authMode} />
-    </div>
-  )
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <AuthForm viewMode={authMode} />
+        </div>
+    );
 }
 
-export default AuthPage
+export default AuthPage;
