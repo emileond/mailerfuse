810edf98dd4777e7e5f3d025483514e20e7b9170
Emilio Sanchez <sonarart@gmail.com>
2025-01-15 18:14:59 -0600

    ui for editing

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -16,54 +16,92 @@ import useCurrentWorkspace from '../hooks/useCurrentWorkspace';
 import {
     useWorkspaceMembers,
     useAddWorkspaceMember,
+    useUpdateWorkspaceMember,
 } from '../hooks/react-query/teams/useWorkspaceMembers';
 import EmptyState from '../components/EmptyState';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import MemberCard from '../components/team/MemberCard';
 import { useUser } from '../hooks/react-query/user/useUser.js';
+import { useState } from 'react';
+import log from 'eslint-plugin-react/lib/util/log.js';
 
 function TeamPage() {
     const { data: user } = useUser();
     const [currentWorkspace] = useCurrentWorkspace();
     const { data: workspaceMembers } = useWorkspaceMembers(currentWorkspace);
     const { mutateAsync: addWorkspaceMember, isPending } = useAddWorkspaceMember(currentWorkspace);
+    const { mutateAsync: updateWorkspaceMember, isPending: isUpdating } =
+        useUpdateWorkspaceMember(currentWorkspace);
     const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+    const [editMember, setEditMember] = useState();
 
     const {
         register,
         handleSubmit,
         reset,
+        setValue,
         formState: { errors },
     } = useForm();
 
     const onSubmit = async (data) => {
-        // Check if email is already invited
-        const isDuplicate = workspaceMembers?.some(
-            (member) => member.invite_email === data.email || member.email === data.email,
-        );
-        if (isDuplicate) {
-            toast.error('This user is already invited');
-        } else {
-            await addWorkspaceMember(
+        if (editMember) {
+            await updateWorkspaceMember(
                 {
-                    invite_email: data.email,
+                    id: editMember.id,
                     role: data.role,
-                    workspace_id: currentWorkspace.workspace_id,
-                    invited_by: user.email,
+                    workspace_id: editMember.workspace_id,
                 },
                 {
                     onSuccess: () => {
-                        toast.success('Team member invited');
+                        toast.success('Team member updated');
                     },
                     onError: (error) => {
                         toast.error(error.message);
                     },
                 },
             );
+        } else {
+            // Check if email is already invited
+            const isDuplicate = workspaceMembers?.some(
+                (member) => member.invite_email === data.email || member.email === data.email,
+            );
+            if (isDuplicate) {
+                toast.error('This user is already invited');
+            } else {
+                await addWorkspaceMember(
+                    {
+                        invite_email: data.email,
+                        role: data.role,
+                        workspace_id: currentWorkspace.workspace_id,
+                        invited_by: user.email,
+                    },
+                    {
+                        onSuccess: () => {
+                            toast.success('Team member invited');
+                        },
+                        onError: (error) => {
+                            toast.error(error.message);
+                        },
+                    },
+                );
+            }
         }
+        setEditMember(null);
         onClose();
         reset();
+    };
+
+    const handleAddMember = () => {
+        reset();
+        setEditMember(null);
+        onOpen();
+    };
+
+    const handleEditMember = (member) => {
+        setValue('email', member.email);
+        setEditMember(member);
+        onOpen();
     };
 
     return (
@@ -73,7 +111,7 @@ function TeamPage() {
                 maxW="2xl"
                 primaryAction="Invite team member"
                 description="Invite team members to your workspace"
-                onClick={onOpen}
+                onClick={handleAddMember}
             >
                 <div className="flex flex-col gap-3 mb-12">
                     <span className="text-sm text-default-600">
@@ -85,7 +123,7 @@ function TeamPage() {
                             <MemberCard
                                 key={member.user_id}
                                 member={member}
-                                onEditMember={(m) => console.log(m)}
+                                onEditMember={(m) => handleEditMember(m)}
                             />
                         ))
                     ) : (
@@ -100,17 +138,22 @@ function TeamPage() {
                 <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
                     <ModalContent>
                         <form onSubmit={handleSubmit(onSubmit)}>
-                            <ModalHeader className="flex flex-col gap-1">
-                                Invite team member
+                            <ModalHeader>
+                                {editMember ? 'Edit team member' : 'Invite team member'}
                             </ModalHeader>
                             <ModalBody>
-                                <p>Enter the email address and role.</p>
+                                <p>
+                                    {editMember
+                                        ? 'Update user role'
+                                        : 'Invite a new member to your workspace'}
+                                </p>
                                 <div className="flex gap-3">
                                     <Input
                                         {...register('email', { required: true })}
                                         label="Email"
                                         type="email"
                                         isInvalid={errors.email}
+                                        isDisabled={editMember}
                                         errorMessage="Email is required"
                                         className="basis-2/3 grow"
                                     />
@@ -118,7 +161,9 @@ function TeamPage() {
                                         {...register('role', { required: true })}
                                         label="Role"
                                         className="basis-1/3"
-                                        defaultSelectedKeys={['member']}
+                                        defaultSelectedKeys={
+                                            editMember ? [editMember.role] : ['member']
+                                        }
                                     >
                                         <SelectItem key="admin" value="admin">
                                             Admin
@@ -134,7 +179,7 @@ function TeamPage() {
                                     Cancel
                                 </Button>
                                 <Button color="primary" type="submit" isLoading={isPending}>
-                                    Send invite
+                                    {editMember ? 'Update' : 'Invite'}
                                 </Button>
                             </ModalFooter>
                         </form>
