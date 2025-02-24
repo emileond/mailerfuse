85d93542d5e6f21fd13d6925b497c7c526de73ba
Emilio Sanchez <sonarart@gmail.com>
2025-02-23 20:47:19 -0600

    form

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -1,14 +1,23 @@
-import AppLayout from '../components/layout/AppLayout'
-import PageLayout from '../components/layout/PageLayout'
+import AppLayout from '../components/layout/AppLayout';
+import PageLayout from '../components/layout/PageLayout';
 
 function IntegrationsPage() {
-  return (
-    <AppLayout>
-      <PageLayout title="Integrations" maxW="3xl">
-        <p>This is the Integrations page.</p>
-      </PageLayout>
-    </AppLayout>
-  )
+    return (
+        <AppLayout>
+            <PageLayout title="Integrations" maxW="3xl">
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSeMGYTjwYJrB9C4K4BPdLGIvrc6s-6LSaWCZmTICIukyE8o5g/viewform?embedded=true"
+                    width="640"
+                    height="1080"
+                    frameBorder="0"
+                    marginHeight="0"
+                    marginWidth="0"
+                >
+                    Loading…
+                </iframe>
+            </PageLayout>
+        </AppLayout>
+    );
 }
 
-export default IntegrationsPage
+export default IntegrationsPage;
