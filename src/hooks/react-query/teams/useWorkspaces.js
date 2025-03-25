47c81877d0032db43b5ee06a4612fd3f6e0ed2e0
Emilio Sanchez <sonarart@gmail.com>
2025-03-25 10:20:15 -0600

    removed create & update react-query hooks

--- src/hooks/react-query/teams/useWorkspaces.js
+++ src/hooks/react-query/teams/useWorkspaces.js
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { supabaseClient } from '../../../lib/supabase';
 
 // Fetch all worskpaces for a user
@@ -40,60 +40,5 @@ export const useWorkspaces = (user) => {
         queryFn: () => fetchWorkspaces(user),
         staleTime: 1000 * 60 * 30, // 30 minutes
         enabled: !!user,
-    });
-};
-
-// Update a team's details
-const updateWorkspace = async ({ workspaceId, name }) => {
-    const { data, error } = await supabaseClient
-        .from('workspaces')
-        .update({ name })
-        .eq('id', workspaceId)
-        .select()
-        .single();
-
-    if (error) {
-        console.error('Error updating workspace:', error);
-        throw error;
-    }
-
-    return data;
-};
-
-// Create a new workspace
-const createWorkspace = async ({ name, user_id }) => {
-    const { data, error } = await supabaseClient
-        .from('workspaces')
-        .insert([{ name, user_id }])
-        .select(); // Optionally, use .select() to return the inserted data
-
-    if (error) {
-        console.error('Error creating workspace:', error);
-        throw error;
-    }
-
-    return data[0];
-};
-
-// Hook to create a new workspace
-export const useCreateWorkspace = () => {
-    const queryClient = useQueryClient();
-
-    return useMutation({
-        mutationFn: createWorkspace,
-        onSuccess: (data) => {
-            // Invalidate the 'workspaces' query to refetch the data
-            queryClient.invalidateQueries(['workspaces', data.user_id]);
-        },
-    });
-};
-
-export const useUpdateWorkspace = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: updateWorkspace,
-        onSuccess: (data) => {
-            queryClient.invalidateQueries(['workspaces', data.user_id]);
-        },
     });
 };
